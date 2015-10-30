d9ddb9d403b730e2a31e8a8b84b399fbdb2532d5
Vasil Stoyanov <vaskosto@abv.bg>
2015-10-30 18:52:35 +0200

    "Scripts" folder introduced, moved JavaScript logic in separate files.

---
+++ scripts/HomeButtonNotification.js
@@ -0,0 +1,17 @@
+﻿// When the home button is pressed 3 times, the inner text will change.
+
+(function () {
+    var pressedTimes = 0;
+    function sayHello() {
+        pressedTimes += 1;
+        if (pressedTimes >= 3) {
+            this.innerText = 'Stop pressing me NOOB';
+        }
+
+        return;
+    }
+
+    var pesho = document.getElementById('home');
+    console.log(pesho);
+    pesho.addEventListener('click', sayHello);
+}());
